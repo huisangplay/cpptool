b8fa5464b6c93fbc7365de72a2337cde6453da48
zhouhui <huisangplay@gmail.com>
2022-10-01 11:03:07 +0800

    添加解析json功能，从配置文件读取

--- imgui-docking/main.cpp
+++ imgui-docking/main.cpp
@@ -1,11 +1,16 @@
+#include<iostream>
+#include<thread>
+#include <fstream>
 #include "imgui/imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
-#include <stdio.h>
-#include<iostream>
-#include<thread>
+
 #include "utils/windowsutil.h"
 #include"utils/stringutil.h"
+#include "utils/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
@@ -16,7 +21,6 @@
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
-using namespace std;
 
 static void glfw_error_callback(int error, const char *description) {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
@@ -26,7 +30,18 @@ static char search_string[256];
 static string search_path;
 
 int main(int, char **) {
+
     SetConsoleOutputCP(CP_UTF8);
+
+    cout<<u8"开始读取配置文件..."<<endl;
+    cout<<u8"支持文件类型为:"<<endl;
+    ifstream fileTypesStream("filetypes.json");
+    json fileTypeJson = json::parse(fileTypesStream);
+    int fileTypesCount=fileTypeJson["filetypes"].size();
+    for(int i=0;i<fileTypesCount;++i){
+        cout<<fileTypeJson["filetypes"][i]<<endl;
+    }
+
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
@@ -83,7 +98,7 @@ int main(int, char **) {
     ImGui_ImplOpenGL3_Init(glsl_version);
 
 
-    io.Fonts->AddFontFromFileTTF("../fonts/SourceHanSansCN-Regular.otf", 25.0f, nullptr,
+    io.Fonts->AddFontFromFileTTF("../fonts/SourceHanSansCN-Regular.otf", 20.0f, nullptr,
                                  io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
     // io.Fonts->AddFontFromFileTTF("Asset/Font/SourceHanSansCN-Normal.otf", 14.0f, nullptr, io.Fonts->GetGlyphRangesChineseFull());
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
@@ -102,9 +117,10 @@ int main(int, char **) {
         {
             static bool fileflag = false;
             string showstr = "show file path";
-            ImGui::Begin("字符串搜索工具");
-            ImGui::InputText(u8"请输入要查询的字符串", search_string, IM_ARRAYSIZE(search_string));
 
+            ImGui::Begin("字符串搜索工具");
+            ImGui::PushItemWidth(500);
+            ImGui::InputText(u8"请输入要查询的字符串", search_string, IM_ARRAYSIZE(search_string));
             if (ImGui::Button(showstr.c_str())) {
                 cout << search_string << endl;
                 //创建一个线程去完成计算动作
