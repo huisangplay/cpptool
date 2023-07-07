c6622cf350fa75c383a3f2b49c06d72f405f6936
huisangplay <huisangplay@gmail.com>
2023-07-07 23:03:01 +0800

    更新docto项目

---
+++ docto_dll_convert/docto.dpr
@@ -0,0 +1,120 @@
+﻿library docto;
+
+{$R ExtraFiles.res}
+
+uses
+  SysUtils,
+  Classes,
+  ActiveX,
+  WordUtils in 'WordUtils.pas',
+  MainUtils in 'MainUtils.pas',
+  ResourceUtils in 'ResourceUtils.pas',
+  PathUtils in 'PathUtils.pas',
+  datamodSSL in 'datamodSSL.pas' {dmSSL: TDataModule} ,
+  ExcelUtils in 'ExcelUtils.pas',
+  PowerPointUtils in 'PowerPointUtils.pas',
+  Word_TLB_Constants in 'Word_TLB_Constants.pas',
+  Excel_TLB_Constants in 'Excel_TLB_Constants.pas',
+  PowerPoint_TLB_Constants in 'PowerPoint_TLB_Constants.pas',
+  VisioUtils in 'VisioUtils.pas',
+  Visio_TLB in 'Visio_TLB.pas';
+
+
+function ConvertDocTo(const inputExtension: PAnsiChar;
+  const inputFile: PAnsiChar; const outputFile: PAnsiChar;
+  const outputFileExtension: PAnsiChar): PAnsiChar; stdcall;
+var
+  Converter: integer;
+  paramlist: TStringlist;
+  DocConv: TWordDocConverter;
+  XLSConv: TExcelXLSConverter;
+  PPConv: TPowerPointConverter;
+  VSConv: TVisioConverter;
+  LogResult: String;
+begin
+
+  paramlist := TStringlist.create;
+
+  try
+    try
+      // Due to the way config is loaded I must create all of them.
+      DocConv := TWordDocConverter.create;
+      XLSConv := TExcelXLSConverter.create;
+      PPConv := TPowerPointConverter.create;
+      VSConv := TVisioConverter.create;
+      try
+        begin
+          paramlist.Add(inputExtension);
+          paramlist.Add('-F');
+          paramlist.Add(inputFile);
+          paramlist.Add('-O');
+          paramlist.Add(outputFile);
+          paramlist.Add('-T');
+          paramlist.Add(outputFileExtension);
+        end;
+
+        CoInitialize(nil);
+
+        Converter := DocConv.ChooseConverter(paramlist);
+
+        // DocConv.LogVersionInfo;
+        if Converter = MSWord then
+        begin
+          DocConv.Log('Converter:MS Word', CHATTY);
+          DocConv.LoadConfig(paramlist);
+          LogResult := DocConv.Execute;
+          DocConv.Log(LogResult);
+        end
+        else if Converter = MSEXCEL then
+        begin
+          // Config Level already set for DocConv but not for XLS.
+          XLSConv.ChooseConverter(paramlist);
+          XLSConv.Log('Converter:MS Excel', CHATTY);
+          XLSConv.LoadConfig(paramlist);
+          LogResult := XLSConv.Execute;
+          XLSConv.Log(LogResult);
+        end
+        ELSE if Converter = MSPOWERPOINT then
+        begin
+          PPConv.ChooseConverter(paramlist);
+          PPConv.Log('Converter:MS Powerpoint', CHATTY);
+          PPConv.LoadConfig(paramlist);
+          LogResult := PPConv.Execute;
+          PPConv.Log(LogResult);
+        end
+        ELSE if Converter = MSVISIO then
+        begin
+          VSConv.ChooseConverter(paramlist);
+          VSConv.Log('Converter:MS Visio', CHATTY);
+          VSConv.LoadConfig(paramlist);
+          LogResult := VSConv.Execute;
+          VSConv.Log(LogResult);
+        end;
+
+        CoUninitialize;
+      finally
+        DocConv.free;
+        XLSConv.free;
+        PPConv.free;
+
+      end;
+    except
+      on E: Exception do
+      begin
+        WriteLn('Exiting with Error 400: ' + E.ClassName);
+        WriteLn(E.Message);
+        halt(400);
+      end;
+
+    end;
+  finally
+    paramlist.Free;
+  end;
+end;
+
+exports
+  ConvertDocTo;
+
+begin
+
+end.
---
+++ docto_dll_convert/main.cpp
@@ -0,0 +1,19 @@
+﻿#pragma comment(lib, "docto.lib")
+
+#include <iostream>
+
+extern "C" char* ConvertDocTo(const char *inputExtension, const char *inputFile, const char *outputFile,
+                                    const char *outputFileExtension);
+
+int main(int argc, char *argv[]) {
+
+    const char *inputExtension = "-WD";
+    const char *inputFile = "test.doc";
+    const char *outputFile = "test.pdf";
+    const char *outputFileExtension = "wdFormatPDF";
+
+    char* result=ConvertDocTo(inputExtension, inputFile, outputFile, outputFileExtension);
+
+    std::cout << 456 << std::endl;
+    return 0;
+}
